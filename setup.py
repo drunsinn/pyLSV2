7bda66e7c3834b0fb8c7c154115f705acd47583e
drunsinn <dr.unsinn@googlemail.com>
2020-10-10 12:56:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name=pyLSV2,
+    packages=find_packages(include=[pyLSV2]),
+    version='0.1.0',
+    description='A pure Python3 implementation of the LSV2 protocol',
+    author='dr.unsinn@googlemail.com',
+    license='MIT',
+    install_requires=[],
+    setup_requires=[],
+    tests_require=[]
+)
