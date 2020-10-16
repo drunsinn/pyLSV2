6b8b2e0daeaa68e6fe3cfddbf92e48ef8e084766
drunsinn <dr.unsinn@googlemail.com>
2020-10-16 11:27:45 +0200

    add test for non binary file recive

--- tests/test_transfer.py
+++ tests/test_transfer.py
@@ -8,12 +8,28 @@ import shutil
 import hashlib
 import pyLSV2
 
+def test_file_recive():
+    lsv2 = pyLSV2.LSV2('192.168.56.101', port=19000, safe_mode=True)
+    lsv2.connect()
+
+    with tempfile.TemporaryDirectory(suffix=None, prefix='pyLSV2_') as tmp_dir_name:
+        local_mdi_path = Path(tmp_dir_name).joinpath('mdi.h')
+        assert lsv2.recive_file(local_path=str(local_mdi_path),
+                                remote_path='TNC:/nc_prog/$mdi.h',
+                                binary_mode=False) == True
+
+        local_tool_table_path = Path(tmp_dir_name).joinpath('tool.t')
+        assert lsv2.recive_file(local_path=str(local_tool_table_path),
+                                remote_path='TNC:/table/tool.t') == True
+
+        lsv2.disconnect()
+
 def test_file_transfer_binary():
     lsv2 = pyLSV2.LSV2('192.168.56.101', port=19000, safe_mode=True)
     lsv2.connect()
 
-    with tempfile.TemporaryDirectory(suffix=None, prefix='pyLSV2_test_') as tmp_dir_name:
-        local_send_path = Path('./tests/testdata.bmp')
+    with tempfile.TemporaryDirectory(suffix=None, prefix='pyLSV2_') as tmp_dir_name:
+        local_send_path = Path('./data/testdata.bmp')
         local_recive_path = Path(tmp_dir_name).joinpath('test.bmp')
         remote_path = 'TNC:/' + local_send_path.name
 
