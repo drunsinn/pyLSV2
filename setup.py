8af87e4ade2b600ee3ac1a0ddbde7c7782e4a03f
drunsinn <dr.unsinn@googlemail.com>
2020-10-17 15:59:43 +0200

    prepare release 0.5.0

--- setup.py
+++ setup.py
@@ -6,12 +6,24 @@ from pyLSV2 import __version__
 
 setup(
     name='pyLSV2',
-    packages=find_packages(include=['pyLSV2']),
+    packages=find_packages(include=['pyLSV2',], exclude=('tests', 'data')),
     version=__version__,
     description='A pure Python3 implementation of the LSV2 protocol',
     long_description=open('README.md').read(),
     author='drunsinn',
     author_email='dr.unsinn@googlemail.com',
+    url='https://github.com/drunsinn/pyLSV2',
     license='MIT',
-    install_requires=[]
+    install_requires=[],
+    scripts=['scripts/check_for_LSV2_commands.py',],
+    zip_safe=True,
+    keywords="LSV2 cnc communication transfer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Topic :: System :: Archiving"
+    ],
+
 )
