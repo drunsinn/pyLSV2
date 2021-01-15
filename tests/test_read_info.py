d388f71920e82ee24d7aeb9d4b8faead0a29003d
drunsinn <max@chp1.net>
2021-01-15 15:23:51 +0100

    Fixed errors in test script

--- tests/test_read_info.py
+++ tests/test_read_info.py
@@ -5,7 +5,7 @@ import pyLSV2
 
 
 def test_read_pgm_status():
-    lsv2 = pyLSV2.LSV2('192.168.56.101', port=19000, safe_mode=True)
+    lsv2 = pyLSV2.LSV2('192.168.56.101', port=19000, safe_mode=False)
     lsv2.connect()
     assert lsv2.get_program_status() != False
     lsv2.disconnect()
@@ -17,7 +17,7 @@ def test_read_pgm_stack():
     lsv2.disconnect()
 
 def test_read_execution_state():
-    lsv2 = pyLSV2.LSV2('192.168.56.101', port=19000, safe_mode=True)
+    lsv2 = pyLSV2.LSV2('192.168.56.101', port=19000, safe_mode=False)
     lsv2.connect()
-    assert lsv2.get_execution_status() != False
+    assert lsv2.get_execution_status() in [0, 1, 2 ,3, 4, 5]
     lsv2.disconnect()
