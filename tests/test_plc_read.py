929cc9e29a51767064208a554150d8e7d11ca774
drunsinn <max@chp1.net>
2021-01-22 17:07:29 +0100

    make address a test parameter

--- tests/test_plc_read.py
+++ tests/test_plc_read.py
@@ -3,8 +3,8 @@
 
 import pyLSV2
 
-def test_login():
-    lsv2 = pyLSV2.LSV2('192.168.56.101', port=19000, safe_mode=False)
+def test_login(address):
+    lsv2 = pyLSV2.LSV2(address, port=19000, safe_mode=False)
     lsv2.connect()
 
     assert lsv2.read_plc_memory(address=0, mem_type=pyLSV2.LSV2.PLC_MEM_TYPE_MARKER, count=1) != False
