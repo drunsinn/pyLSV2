b072deacc3e49356201c723df3cc1c6480a25d0e
drunsinn <max@chp1.net>
2021-04-25 20:27:30 +0200

    styling

--- pyLSV2/__init__.py
+++ pyLSV2/__init__.py
@@ -3,5 +3,7 @@
 """A pure Python3 implementation of the LSV2 protocol"""
 from .client import LSV2
 from .const import *
+from .translate_messages import (get_error_text, get_execution_status_text,
+                                 get_program_status_text)
 
 __version__ = '0.6.4'
