06c0942cfc9af862dacc69ffcd6b4642f97d2728
drunsinn <max@chp1.net>
2021-04-25 13:20:30 +0200

    Update test_connection.py

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -9,9 +9,9 @@ def test_login(address, timeout):
     """test to see if login and safe mode works"""
     lsv2 = pyLSV2.LSV2(address, port=19000, timeout=timeout, safe_mode=True)
     lsv2.connect()
-    assert lsv2.login(login=pyLSV2.LSV2.LOGIN_INSPECT) is True
-    assert lsv2.login(login=pyLSV2.LSV2.LOGIN_FILETRANSFER) is True
-    assert lsv2.login(login=pyLSV2.LSV2.LOGIN_DNC) is False
+    assert lsv2.login(login=pyLSV2.LOGIN_INSPECT) is True
+    assert lsv2.login(login=pyLSV2.LOGIN_FILETRANSFER) is True
+    assert lsv2.login(login=pyLSV2.LOGIN_DNC) is False
     lsv2.disconnect()
 
 
