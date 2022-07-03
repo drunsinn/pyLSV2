2a71f6b59876d6c3ec3062bc130c062333c2f0f2
Max <max@chp1.net>
2022-07-03 17:44:54 +0200

    mimic traffic from wireshark capture

--- scripts/itnc_transfer_test.py
+++ scripts/itnc_transfer_test.py
@@ -10,36 +10,85 @@ from pyLSV2.const import CMD, RSP, ParCCC
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    address = "localhost"
+    address = "192.168.56.101"
     if len(sys.argv) > 1:
         address = sys.argv[1]
-
     print("Connecting to {}".format(address))
 
     logging.basicConfig(level=logging.DEBUG)
-    con = pyLSV2.LSV2("localhost", port=19000, timeout=5, safe_mode=False)
 
-    con.connect()
+    con = pyLSV2.LSV2(address, port=19000, timeout=5, safe_mode=False)
 
-    # force lower buffer size
-    con.set_system_command(ParCCC.SET_BUF2048)
-    con._buffer_size = 2048
-    print("Connection uses buffer size {}".format(con._buffer_size))
+    con._llcom.connect()
 
-    s_st_content = con._send_recive(command="R_ST", expected_response="S_ST", payload=None)
-    print("response to R_ST was : {}".format(s_st_content))
+    # login INSPECT
+    payload = bytearray()
+    payload.extend(map(ord, "INSPECT"))
+    payload.append(0x00)
+    result = con._send_recive(command="A_LG", expected_response="T_OK", payload=payload)
 
-    con.change_directory("TNC:/")
+    # read info
+    result = con._send_recive(command="R_VR", expected_response="S_VR", payload=None)
 
+    # read parameters
+    result = con._send_recive(command="R_PR", expected_response="S_PR", payload=None)
+
+    # set system par 0x00 0x03 -> set buffer 1024
+    payload = bytearray()
+    payload.append(0x00)
+    payload.append(0x03)
+    result = con._send_recive(command="C_CC", expected_response="T_OK", payload=payload)
+    con._buffer_size = 1024
+
+    # set system par 0x00 0x06  -> set buffer 3075
+    payload = bytearray()
+    payload.append(0x00)
+    payload.append(0x06)
+    result = con._send_recive(command="C_CC", expected_response="T_OK", payload=payload)
+    con._buffer_size = 3075
+
+    # login FILE
+    payload = bytearray()
+    payload.extend(map(ord, "FILE"))
+    payload.append(0x00)
+    result = con._send_recive(command="A_LG", expected_response="T_OK", payload=payload)
+
+    # r_st
+    result = con._send_recive(command="R_ST", expected_response="S_ST", payload=None)
+    
+    # read info 0x05 -> ID
+    payload = bytearray()
+    payload.append(0x05)
+    result = con._send_recive(command="R_VR", expected_response="S_VR", payload=payload)
+
+    # read dir info
+    result = con._send_recive(command="R_DI", expected_response="S_DI", payload=None)
+
+    # read dir content 0x01 -> multible entries at a time
+    payload = bytearray()
+    payload.append(0x01)
+    result = con._send_recive_block(command="R_DR", expected_response="S_DR", payload=payload)
+
+    # r_st
+    result = con._send_recive(command="R_ST", expected_response="S_ST", payload=None)
+    
+    time.sleep(3)
+
+    # r_st
+    result = con._send_recive(command="R_ST", expected_response="S_ST", payload=None)
+    
+    time.sleep(3)
+
+    # r_st
+    result = con._send_recive(command="R_ST", expected_response="S_ST", payload=None)
+    
+    # load file
     payload = bytearray()
     payload.extend(map(ord, "TNC:/TOOL_P.TCH"))
     payload.append(0x00)
-
     response, content = con._llcom.telegram(CMD.R_FL, payload, buffer_size=con._buffer_size)
     
     byte_counter = len(content)
-
-    time.sleep(1)
     
     print("response to R_FL was : '{}' content is {} byts long".format(response, byte_counter))
     if response == RSP.S_FL:
@@ -60,10 +109,5 @@ if __name__ == "__main__":
 
     content = con._send_recive(command="R_ST", expected_response="S_ST", payload=None)
     print("response to R_ST was : {}".format(content))
-
-    if s_st_content is content:
-        print("both S_ST respones match")
-    else:
-        print("both S_ST respones do not match")
 
     con.disconnect()
