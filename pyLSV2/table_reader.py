d9c7260d244daf653e476ee879e3e56953007971
Max <max@chp1.net>
2022-07-25 19:44:23 +0200

    add typing to table reader

--- pyLSV2/table_reader.py
+++ pyLSV2/table_reader.py
@@ -1,10 +1,228 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """module with reader and writer for TNC tables"""
+from typing import Union
 import json
 import logging
+import pathlib
 import re
-from pathlib import Path
+
+
+class NCTabel:
+    """generic object for table files commonly used by TTNC, iTNC, CNCPILOT,
+    MANUALplus and 6000i CNC
+    """
+
+    def __init__(
+        self, name: str = "", suffix: str = "", version: str = "", has_unit: bool = False, is_metric: bool = False
+    ):
+        """init object variables logging"""
+        logging.getLogger(__name__).addHandler(logging.NullHandler())
+        self.name = name
+        self.suffix = suffix
+        self.version = version
+        self.has_unit = has_unit
+        self.is_metric = is_metric
+        self._content = []
+        self._columns = []
+        self._column_format = {}
+
+    @property
+    def name(self) -> str:
+        """Name of the table read from the header"""
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+
+    @property
+    def suffix(self) -> str:
+        """file suffix of table"""
+        return self._suffix
+
+    @suffix.setter
+    def suffix(self, value: str):
+        if value is None:
+            self._suffix = None
+        else:
+            self._suffix = value.lower()
+
+    @property
+    def version(self) -> str:
+        """version string of from table header"""
+        return self._version
+
+    @version.setter
+    def version(self, value: str):
+        self._version = value
+
+    @property
+    def has_unit(self):
+        """identifies of values in table are dependent on measuring units"""
+        return self._has_unit
+
+    @has_unit.setter
+    def has_unit(self, value: bool):
+        self._has_unit = value
+
+    @property
+    def is_metric(self) -> bool:
+        """if true all values should be interpreted as metric"""
+        return self._is_metric
+
+    @is_metric.setter
+    def is_metric(self, value: bool):
+        self._is_metric = value
+
+    def append_column(self, name: str, start: int, end: int, width: int = 0, empty_value=None):
+        """add column to the table format"""
+        self._columns.append(name)
+        if width == 0:
+            width = end - start
+        self._column_format[name] = {
+            "width": width,
+            "start": start,
+            "end": end,
+        }
+        if empty_value is not None:
+            self._column_format[name]["empty_value"] = empty_value
+
+    def remove_column(self, name: str):
+        """remove column by name from table format"""
+        self._columns.remove(name)
+        del self._column_format[name]
+
+    def get_column_start(self, name: str):
+        """get start index of column"""
+        return self._column_format[name]["start"]
+
+    def get_column_end(self, name: str):
+        """get end index of column"""
+        return self._column_format[name]["end"]
+
+    def get_column_width(self, name: str):
+        """get width if column"""
+        return self._column_format[name]["width"]
+
+    def get_column_empty_value(self, name: str):
+        """get value define as default value for column"""
+        if "empty_value" in self._column_format[name]:
+            return self._column_format[name]["empty_value"]
+        else:
+            return None
+
+    def set_column_empty_value(self, name, value):
+        """set the default value of a column"""
+        if len(str(value)) > self._column_format[name]["width"]:
+            raise Exception("value to long for column")
+        self._column_format[name]["empty_value"] = value
+
+    def get_column_names(self):
+        """get list of columns used in this table"""
+        return self._columns
+
+    def append_row(self, row):
+        """add a data entry to the table"""
+        self._content.append(row)
+
+    def extend_rows(self, rows):
+        """add multiple data entries at onec"""
+        self._content.extend(rows)
+
+    @property
+    def rows(self):
+        """data entries in this table"""
+        return self._content
+
+    def format_to_json(self) -> str:
+        """return json configuration representing the table format"""
+        json_data = {}
+        json_data["version"] = self.version
+        json_data["suffix"] = self.suffix
+        json_data["column_list"] = self.get_column_names()
+        json_data["column_config"] = self._column_format
+        return json.dumps(json_data, ensure_ascii=False, indent=2)
+
+    @staticmethod
+    def from_json(file_path: pathlib.Path) -> 'NCTabel':
+        """return a new NCTable object based on a json configuration file"""
+        nct = NCTabel()
+        with open(file_path, "r", encoding="utf-8") as jfp:
+            json_data = json.load(jfp)
+            nct.version = json_data["version"]
+            nct.suffix = json_data["suffix"]
+            for column in json_data["column_config"]:
+                nct.append_column(
+                    name=column,
+                    start=json_data["column_config"][column]["start"],
+                    end=json_data["column_config"][column]["end"],
+                )
+                if "empty_value" in json_data["column_config"][column]:
+                    nct.set_column_empty_value(
+                        column, json_data["column_config"][column]["empty_value"]
+                    )
+        return nct
+
+    def dump(self, file_path: pathlib.Path, renumber_column=None):
+        """write table data to a file in the format used by the controls"""
+        row_counter = 0
+        file_name = file_path.name.upper()
+
+        units_string = ""
+        if self._has_unit:
+            if self._is_metric:
+                units_string = " MM"
+            else:
+                units_string = " INCH"
+        version_string = ""
+        if self._version is not None:
+            version_string = " Version:%s" % str(self._version)
+
+        with open(file_path, "w", encoding="ascii") as tfp:
+            tfp.write("BEGIN %s%s%s\n" %
+                      (file_name, units_string, version_string))
+
+            for column_name in self._columns:
+                if column_name not in self._column_format:
+                    raise Exception(
+                        "configuration is incomplete, missing definition for column {column_name:s}"
+                    )
+                fixed_width = self._column_format[column_name]["width"]
+                format_string = "{0:<%d}" % fixed_width
+                tfp.write(format_string.format(column_name))
+            tfp.write("\n")
+
+            for row in self._content:
+                for column_name in self._columns:
+                    fixed_width = self._column_format[column_name]["width"]
+                    format_string = "{0:<%d}" % fixed_width
+
+                    if column_name is renumber_column:
+                        tfp.write(format_string.format(row_counter))
+                    else:
+                        if column_name in row:
+                            tfp.write(format_string.format(row[column_name]))
+                        else:
+                            if "empty_value" in self._column_format[column_name]:
+                                logging.warning(
+                                    "entry is missing optional column %s defined in output format, replace with empty value",
+                                    column_name,
+                                )
+                                tfp.write(
+                                    format_string.format(
+                                        self._column_format[column_name]["empty_value"]
+                                    )
+                                )
+                            else:
+                                raise Exception(
+                                    "entry is missing a value for column %s defined in the output format"
+                                    % column_name
+                                )
+                tfp.write("\n")
+                row_counter += 1
+
+            tfp.write("[END]\n")
 
 
 class TableReader:
@@ -16,7 +234,7 @@ class TableReader:
         """init object variables logging"""
         logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-    def parse_table(self, table_path):
+    def parse_table(self, table_path: pathlib.Path) -> NCTabel:
         """Parse a file of one of the common table formats
 
         :param str or Path table_path: Path to the table file
@@ -26,7 +244,7 @@ class TableReader:
         """
         nctable = NCTabel()
 
-        table_file = Path(table_path)
+        table_file = pathlib.Path(table_path)
         if not table_file.is_file():
             raise FileNotFoundError("Could not open file %s" % table_path)
 
@@ -105,7 +323,8 @@ class TableReader:
                         end=column_match.end(),
                     )
 
-                logging.debug("Found %d columns", len(nctable.get_column_names()))
+                logging.debug("Found %d columns", len(
+                    nctable.get_column_names()))
 
                 for line in tfp.readlines():
                     if line.startswith("[END]"):
@@ -114,7 +333,7 @@ class TableReader:
                     table_entry = {}
                     for column in nctable.get_column_names():
                         table_entry[column] = line[
-                            nctable.get_column_start(column) : nctable.get_column_end(
+                            nctable.get_column_start(column): nctable.get_column_end(
                                 column
                             )
                         ].strip()
@@ -126,7 +345,7 @@ class TableReader:
         return nctable
 
     @staticmethod
-    def format_entry_float(str_value):
+    def format_entry_float(str_value: str) -> Union[float, None]:
         """convert the string value of a table cell to float value"""
         str_value = str_value.strip()
         if str_value == "-" or len(str_value) == 0:
@@ -135,7 +354,7 @@ class TableReader:
         return float(str_value)
 
     @staticmethod
-    def format_entry_int(str_value):
+    def format_entry_int(str_value: str) -> Union[int, None]:
         """convert the string value of a table cell to int value"""
         str_value = str_value.strip()
         if str_value == "-" or len(str_value) == 0:
@@ -143,7 +362,7 @@ class TableReader:
         return int(str_value)
 
     @staticmethod
-    def format_entry_bool(str_value):
+    def format_entry_bool(str_value: str) -> Union[bool, None]:
         """convert the string value of a table cell to boolean value"""
         str_value = str_value.strip()
         if len(str_value) == 0:
@@ -151,219 +370,3 @@ class TableReader:
         elif str_value == "1":
             return True
         return False
-
-
-class NCTabel:
-    """generic object for table files commonly used by TTNC, iTNC, CNCPILOT,
-    MANUALplus and 6000i CNC
-    """
-
-    def __init__(
-        self, name=None, suffix=None, version=None, has_unit=False, is_metric=False
-    ):
-        """init object variables logging"""
-        logging.getLogger(__name__).addHandler(logging.NullHandler())
-        self.name = name
-        self.suffix = suffix
-        self.version = version
-        self.has_unit = has_unit
-        self.is_metric = is_metric
-        self._content = []
-        self._columns = []
-        self._column_format = {}
-
-    @property
-    def name(self):
-        """Name of the table read from the header"""
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        self._name = value
-
-    @property
-    def suffix(self):
-        """file suffix of table"""
-        return self._suffix
-
-    @suffix.setter
-    def suffix(self, value):
-        if value is None:
-            self._suffix = None
-        else:
-            self._suffix = value.lower()
-
-    @property
-    def version(self):
-        """version string of from table header"""
-        return self._version
-
-    @version.setter
-    def version(self, value):
-        self._version = value
-
-    @property
-    def has_unit(self):
-        """identifies of values in table are dependent on measuring units"""
-        return self._has_unit
-
-    @has_unit.setter
-    def has_unit(self, value):
-        self._has_unit = value
-
-    @property
-    def is_metric(self):
-        """if true all values should be interpreted as metric"""
-        return self._is_metric
-
-    @is_metric.setter
-    def is_metric(self, value):
-        self._is_metric = value
-
-    def append_column(self, name, start, end, width=0, empty_value=None):
-        """add column to the table format"""
-        self._columns.append(name)
-        if width == 0:
-            width = end - start
-        self._column_format[name] = {
-            "width": int(width),
-            "start": int(start),
-            "end": int(end),
-        }
-        if empty_value is not None:
-            self._column_format[name]["empty_value"] = empty_value
-
-    def remove_column(self, name):
-        """remove column by name from table format"""
-        self._columns.remove(name)
-        del self._column_format[name]
-
-    def get_column_start(self, name):
-        """get start index of column"""
-        return self._column_format[name]["start"]
-
-    def get_column_end(self, name):
-        """get end index of column"""
-        return self._column_format[name]["end"]
-
-    def get_column_width(self, name):
-        """get width if column"""
-        return self._column_format[name]["width"]
-
-    def get_column_empty_value(self, name):
-        """get value define as default value for column"""
-        if "empty_value" in self._column_format[name]:
-            return self._column_format[name]["empty_value"]
-        else:
-            return None
-
-    def set_column_empty_value(self, name, value):
-        """set the default value of a column"""
-        if len(str(value)) > self._column_format[name]["width"]:
-            raise Exception("value to long for column")
-        self._column_format[name]["empty_value"] = value
-
-    def get_column_names(self):
-        """get list of columns used in this table"""
-        return self._columns
-
-    def append_row(self, row):
-        """add a data entry to the table"""
-        self._content.append(row)
-
-    def extend_rows(self, rows):
-        """add multiple data entries at onec"""
-        self._content.extend(rows)
-
-    @property
-    def rows(self):
-        """data entries in this table"""
-        return self._content
-
-    def format_to_json(self):
-        """return json configuration representing the table format"""
-        json_data = {}
-        json_data["version"] = self.version
-        json_data["suffix"] = self.suffix
-        json_data["column_list"] = self.get_column_names()
-        json_data["column_config"] = self._column_format
-        return json.dumps(json_data, ensure_ascii=False, indent=2)
-
-    @staticmethod
-    def from_json(file_path):
-        """return a new NCTable object based on a json configuration file"""
-        with open(file_path, "r", encoding="utf-8") as jfp:
-            json_data = json.load(jfp)
-            nct = NCTabel()
-            nct.version = json_data["version"]
-            nct.suffix = json_data["suffix"]
-            for column in json_data["column_config"]:
-                nct.append_column(
-                    name=column,
-                    start=json_data["column_config"][column]["start"],
-                    end=json_data["column_config"][column]["end"],
-                )
-                if "empty_value" in json_data["column_config"][column]:
-                    nct.set_column_empty_value(
-                        column, json_data["column_config"][column]["empty_value"]
-                    )
-        return nct
-
-    def dump(self, file_path, renumber_column=None):
-        """write table data to a file in the format used by the controls"""
-        row_counter = 0
-        file_name = file_path.name.upper()
-
-        units_string = ""
-        if self._has_unit:
-            if self._is_metric:
-                units_string = " MM"
-            else:
-                units_string = " INCH"
-        version_string = ""
-        if self._version is not None:
-            version_string = " Version:%s" % str(self._version)
-
-        with open(file_path, "w", encoding="ascii") as tfp:
-            tfp.write("BEGIN %s%s%s\n" % (file_name, units_string, version_string))
-
-            for column_name in self._columns:
-                if column_name not in self._column_format:
-                    raise Exception(
-                        "configuration is incomplete, missing definition for column {column_name:s}"
-                    )
-                fixed_width = self._column_format[column_name]["width"]
-                format_string = "{0:<%d}" % fixed_width
-                tfp.write(format_string.format(column_name))
-            tfp.write("\n")
-
-            for row in self._content:
-                for column_name in self._columns:
-                    fixed_width = self._column_format[column_name]["width"]
-                    format_string = "{0:<%d}" % fixed_width
-
-                    if column_name is renumber_column:
-                        tfp.write(format_string.format(row_counter))
-                    else:
-                        if column_name in row:
-                            tfp.write(format_string.format(row[column_name]))
-                        else:
-                            if "empty_value" in self._column_format[column_name]:
-                                logging.warning(
-                                    "entry is missing optional column %s defined in output format, replace with empty value",
-                                    column_name,
-                                )
-                                tfp.write(
-                                    format_string.format(
-                                        self._column_format[column_name]["empty_value"]
-                                    )
-                                )
-                            else:
-                                raise Exception(
-                                    "entry is missing a value for column %s defined in the output format"
-                                    % column_name
-                                )
-                tfp.write("\n")
-                row_counter += 1
-
-            tfp.write("[END]\n")
