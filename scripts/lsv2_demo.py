f9bc24cfce6854788fa76332258e3f634d053335
Max <max@chp1.net>
2022-10-24 19:53:15 +0200

    update demo script to use context manager

--- scripts/lsv2_demo.py
+++ scripts/lsv2_demo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """This script contains examples on how to use different functions of pyLSV2
-   Not all functions are shown here, especially the file functions aren't shown here
+   Not all functions are shown here
 """
 import argparse
 import logging
@@ -12,211 +12,205 @@ from pathlib import Path
 import pyLSV2
 from pyLSV2.const import MemoryType
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('address', nargs='?',
-                        default='192.168.56.101', type=str)
+    parser.add_argument("address", nargs="?", default="192.168.56.101", type=str)
 
     parser.add_argument(
-        '-d', '--debug',
+        "-d",
+        "--debug",
         help="Print lots of debugging statements",
-        action="store_const", dest="loglevel", const=logging.DEBUG,
+        action="store_const",
+        dest="loglevel",
+        const=logging.DEBUG,
         default=logging.WARNING,
     )
     parser.add_argument(
-        '-v', '--verbose',
+        "-v",
+        "--verbose",
         help="Be verbose",
-        action="store_const", dest="loglevel", const=logging.INFO,
+        action="store_const",
+        dest="loglevel",
+        const=logging.INFO,
     )
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel)
 
     print("Connecting to {}".format(args.address))
 
-    con = pyLSV2.LSV2(args.address, port=19000, timeout=5, safe_mode=False)
-    con.connect()
+    with pyLSV2.LSV2(args.address, port=19000, timeout=5) as con:
 
-    print(
-        'Connected to "{Control}" with NC Software "{NC_Version}"'.format(
-            **con._read_version()
-        )
-    )
-    print(
-        "Running LSV2 Version {LSV2_Version} with Flags {LSV2_Version_Flags}".format(
-            **con._read_parameters()
-        )
-    )
+        con = pyLSV2.LSV2(args.address, port=19000, timeout=5, safe_mode=False)
+        con.connect()
 
-    print("Drive Info: {}".format(con.get_drive_info()))
-    print(
-        "Current Folder {Path} Free Space: {Free Size} Attrib: {Dir_Attributs}".format(
-            **con.get_directory_info()
-        )
-    )
-
-    print("Axes positions: {}".format(con.get_axes_location()))
-
-    print(
-        "PLC Marker: {}".format(
-            con.read_plc_memory(
-                address=0, mem_type=MemoryType.MARKER, count=15)
-        )
-    )
-    print(
-        "PLC Word: {}".format(
-            con.read_plc_memory(address=6, mem_type=MemoryType.WORD, count=10)
-        )
-    )
-    print(
-        "PLC Double Word: {}".format(
-            con.read_plc_memory(address=0, mem_type=MemoryType.DWORD, count=10)
-        )
-    )
-    print(
-        "PLC String: {}".format(
-            con.read_plc_memory(address=2, mem_type=MemoryType.STRING, count=2)
-        )
-    )
-    print(
-        "PLC Input: {}".format(
-            con.read_plc_memory(address=0, mem_type=MemoryType.INPUT, count=5)
-        )
-    )
-    print(
-        "PLC Word Output: {}".format(
-            con.read_plc_memory(
-                address=10, mem_type=MemoryType.OUTPUT_WORD, count=5)
-        )
-    )
-
-    # read values from PLC and other locations with telegram R_DP. Only works on iTNC
-    if con.is_itnc():
+        print("Basics:")
         print(
-            "PLC Double Word 7928: {}".format(
-                con.read_data_path("/PLC/memory/D/7928"))
-        )
-        print("PLC Constant 1: {}".format(
-            con.read_data_path("/PLC/memory/K/1")))
-        print("PLC Marker 211: {}".format(
-            con.read_data_path("/PLC/memory/M/211")))
-        print("PLC Byte 7192: {}".format(
-            con.read_data_path("/PLC/memory/B/7192")))
-        print("PLC Word 10908: {}".format(
-            con.read_data_path("/PLC/memory/W/10908")))
-        print("PLC Input 11: {}".format(con.read_data_path("/PLC/memory/I/11")))
-        print("PLC String 30: {}".format(
-            con.read_data_path("/PLC/memory/S/30")))
-        print(
-            "DOC column of tool 1: {}".format(
-                con.read_data_path("/TABLE/TOOL/T/1/DOC"))
-        )
-        print("L column of tool 1: {}".format(
-            con.read_data_path("/TABLE/TOOL/T/1/L")))
-        print("R column of tool 1: {}".format(
-            con.read_data_path("/TABLE/TOOL/T/1/R")))
-        print(
-            "PLC column of tool 1: {}".format(
-                con.read_data_path("/TABLE/TOOL/T/1/PLC"))
-        )
-        print(
-            "TMAT column of tool 1: {}".format(
-                con.read_data_path("/TABLE/TOOL/T/1/TMAT")
+            "# Connected to a '{:s}' running software version '{:s}'".format(
+                con.versions.control_version, con.versions.nc_version
             )
         )
         print(
-            "LAST_USE column of tool 1: {}".format(
-                con.read_data_path("/TABLE/TOOL/T/1/LAST_USE")
+            "# Using LSV2 version '{:d}' with version flags '0x{:02x}' and '0x{:02x}'".format(
+                con.parameters.lsv2_version,
+                con.parameters.lsv2_version_flags,
+                con.parameters.lsv2_version_flags_ex,
             )
         )
 
-    # reading of machine parameter for old an new style names
-    if not con.is_itnc():
-        # new stype
-        print(
-            "Current Language: {}".format(
-                con.get_machine_parameter("CfgDisplayLanguage.ncLanguage")
-            )
-        )
-    else:
-        # old style
-        print("Current Language: {}".format(
-            con.get_machine_parameter("7230.0")))
-
-    # changing the value of a machine parameter
-    # con.login(pyLSV2.Login.PLCDEBUG)
-    # new style: con.set_machine_parameter('CfgDisplayLanguage.ncLanguage', 'CZECH', safe_to_disk=False)
-    # old style: con.set_machine_parameter('7230.0', '2', safe_to_disk=False)
-    # con.logout(pyLSV2.Login.PLCDEBUG)
-
-    # read program stack
-    print("Current Program Stack: {}".format(con.get_program_stack()))
-
-    # demo for sending key codes
-    con.login(pyLSV2.Login.MONITOR)
-    con.set_keyboard_access(False)
-    con.send_key_code(pyLSV2.KeyCode.MODE_MANUAL)
-    time.sleep(3)
-    con.send_key_code(pyLSV2.KeyCode.MODE_PGM_EDIT)
-    con.set_keyboard_access(True)
-    con.logout(pyLSV2.Login.MONITOR)
-
-    # demo for reading the current tool with fallback if it is not supported by control
-    t_info = con.get_spindle_tool_status()
-    if t_info is not False:
-        print(
-            "Current tool number {Number}.{Index} with Axis {Axis} Length {Length} Radius {Radius}".format(
-                **t_info
-            )
-        )
-    else:
-        print(
-            "Direct reading to current tool not supported for this control type. using backup strategy"
-        )
-        if con.is_tnc():
-            pocket_table_path = "TNC:/table/tool_p.tch"
-            transfer_binary = True
-            spindel_lable = "0.0"
-        elif con.is_itnc():
-            pocket_table_path = "TNC:/TOOL_P.TCH"
-            transfer_binary = False
-            spindel_lable = "0"
+        # read error messages via LSV2, works only on iTNC controls
+        print("# read error messages, only availible on iTNC530")
+        if con.versions.is_itnc():
+            e_m = con.get_error_messages()
+            print("## Number of currently active error messages: {:d}".format(len(e_m)))
+            for i, msg in enumerate(e_m):
+                print("### Error {:d} : {:s}".format(i, str(msg)))
         else:
-            pocket_table_path = "TNC:/table/ToolAllo.tch"
-            transfer_binary = True
-            spindel_lable = "0.0"
+            print("## function 'get_error_messages()' not suportet for this control")
 
-        with tempfile.TemporaryDirectory(suffix=None, prefix="pyLSV2_") as tmp_dir_name:
-            local_recive_path = Path(tmp_dir_name).joinpath("tool_p.tch")
-            con.recive_file(
-                local_path=str(local_recive_path),
-                remote_path=pocket_table_path,
-                binary_mode=transfer_binary,
-            )
-            tr = pyLSV2.TableReader()
-            pockets = tr.parse_table(local_recive_path)
-            spindel = list(
-                filter(lambda pocket: pocket["P"] == spindel_lable, pockets)
-            )[0]
-            print(
-                "Current tool number {T} with name {TNAME}".format(**spindel))
-
-    # read error messages via LSV2, works only on iTNC controls
-    if con.is_itnc():
-        e_m = con.get_error_messages()
+        print("Machine:")
+        print("# axes positions: {}".format(con.get_axes_location()))
+        exec_stat = con.get_execution_status()
+        exec_stat_text = pyLSV2.get_execution_status_text(exec_stat)
+        print("# execution: {:d} - '{:s}'".format(exec_stat, exec_stat_text))
+        pgm_stat = con.get_program_status()
+        pgm_stat_text = pyLSV2.get_program_status_text(pgm_stat)
+        print("# program: {:d} - '{:s}'".format(pgm_stat, pgm_stat_text))
+        pgm_stack = con.get_program_stack()
+        print("# selected program: '{:s}'".format(pgm_stack.main_pgm))
         print(
-            "Number of currently ative error messages: {:d}".format(len(e_m)))
-        for i, msg in enumerate(e_m):
-            print("Error {:d} : {:s}".format(i, msg["Text"]))
+            "## currently execution '{:s}' on line {:d}".format(
+                pgm_stack.current_pgm, pgm_stack.current_line
+            )
+        )
 
-    # list all NC-Programms in TNC partition
-    h_files = con.get_file_list(
-        path="TNC:", pattern=r"[\$A-Za-z0-9_-]*\.[hH]$")
-    print("Found {:d} Klartext programs: {:}".format(len(h_files), h_files))
-    i_files = con.get_file_list(
-        path="TNC:", pattern=r"[\$A-Za-z0-9_-]*\.[iI]$")
-    print("Found {:d} DIN/ISO programs: {:}".format(len(i_files), i_files))
+        print("PLC memory:")
+        print("# the first 5 entries for some memory types:")
+        print("## marker: {}".format(con.read_plc_memory(0, MemoryType.MARKER, 5)))
+        print("## word: {}".format(con.read_plc_memory(0, MemoryType.WORD, 5)))
+        print("## double word: {}".format(con.read_plc_memory(0, MemoryType.DWORD, 5)))
+        print("## string: {}".format(con.read_plc_memory(0, MemoryType.STRING, 5)))
+        print("## input: {}".format(con.read_plc_memory(0, MemoryType.INPUT, 5)))
+        print("## output: {}".format(con.read_plc_memory(0, MemoryType.OUTPUT_WORD, 5)))
 
-    con.disconnect()
+        print("# data values via data path, only availible on iTNC530")
+        if con.versions.is_itnc():
+            print("## marker 0: {}".format(con.read_data_path("/PLC/memory/M/0")))
+            print("## marker 1: {}".format(con.read_data_path("/PLC/memory/M/1")))
+            print("## string 0: {}".format(con.read_data_path("/PLC/memory/S/0")))
+            print("## word 10908: {}".format(con.read_data_path("/PLC/memory/W/10908")))
+        else:
+            print("## function 'read_data_path()' not suportet for this control")
+        print("# table values via data path, only availible on iTNC530")
+        if con.versions.is_itnc():
+            print("## values from tool table for tool T1:")
+            print("## DOC column: {}".format(con.read_data_path("/TABLE/TOOL/T/1/DOC")))
+            print("## L column: {}".format(con.read_data_path("/TABLE/TOOL/T/1/L")))
+            print("## R column: {}".format(con.read_data_path("/TABLE/TOOL/T/1/R")))
+        else:
+            print("## function 'read_data_path()' not suportet for this control")
+
+        print("Configuration:")
+        if con.versions.is_itnc():
+            # old style
+            lang = con.get_machine_parameter("7230.0")
+        else:
+            # new style
+            lang = con.get_machine_parameter("CfgDisplayLanguage.ncLanguage")
+        print("# Value of machine parameter for NC language: {:s}".format(lang))
+
+        print("UI Interface")
+        print("# switch to mode manual")
+        con.set_keyboard_access(False)
+        con.send_key_code(pyLSV2.KeyCode.MODE_MANUAL)
+        con.set_keyboard_access(True)
+        print("# wait 5 seconds")
+        time.sleep(5)
+        print("# switch to mode edit")
+        con.set_keyboard_access(False)
+        con.send_key_code(pyLSV2.KeyCode.MODE_PGM_EDIT)
+        con.set_keyboard_access(True)
+
+        print("File access")
+        drv_info = con.get_drive_info()
+        drv_names = ", ".join([drv.name for drv in drv_info])
+        print("# names of disk drives: {:s}".format(drv_names))
+        dir_info = con.get_directory_info()
+        print(
+            "# current directory is '{:s}' with {:d} bytes of free drive space".format(
+                dir_info.path, dir_info.free_size
+            )
+        )
+
+        dir_content = con.get_directory_content()
+        only_files = filter(
+            lambda f_e: f_e.is_directory is False and f_e.is_drive is False,
+            dir_content,
+        )
+
+        for file_entry in only_files:
+            print(
+                "## file name: {:s}, date {:}, size {:d} bytes".format(
+                    file_entry.name, file_entry.timestamp, file_entry.size
+                )
+            )
+        only_dir = filter(
+            lambda f_e: f_e.is_directory is True and f_e.is_drive is False, dir_content
+        )
+        for file_entry in only_dir:
+            print(
+                "## directory name: {:s}, date {:}".format(
+                    file_entry.name, file_entry.timestamp
+                )
+            )
+
+        print("# file search")
+        h_files = con.get_file_list(path="TNC:", pattern=r"[\$A-Za-z0-9_-]*\.[hH]$")
+        print("## found {:d} klartext programs on TNC drive".format(len(h_files)))
+        i_files = con.get_file_list(path="TNC:", pattern=r"[\$A-Za-z0-9_-]*\.[iI]$")
+        print("## found {:d} ISO programs on TNC drive".format(len(i_files)))
+
+        exit()
+
+        # demo for reading the current tool with fallback if it is not supported by control
+        t_info = con.get_spindle_tool_status()
+        if t_info is not False:
+            print(
+                "Current tool number {Number}.{Index} with Axis {Axis} Length {Length} Radius {Radius}".format(
+                    **t_info
+                )
+            )
+        else:
+            print(
+                "Direct reading to current tool not supported for this control type. using backup strategy"
+            )
+            if con.is_tnc():
+                pocket_table_path = "TNC:/table/tool_p.tch"
+                transfer_binary = True
+                spindel_lable = "0.0"
+            elif con.is_itnc():
+                pocket_table_path = "TNC:/TOOL_P.TCH"
+                transfer_binary = False
+                spindel_lable = "0"
+            else:
+                pocket_table_path = "TNC:/table/ToolAllo.tch"
+                transfer_binary = True
+                spindel_lable = "0.0"
+
+            with tempfile.TemporaryDirectory(
+                suffix=None, prefix="pyLSV2_"
+            ) as tmp_dir_name:
+                local_recive_path = Path(tmp_dir_name).joinpath("tool_p.tch")
+                con.recive_file(
+                    local_path=str(local_recive_path),
+                    remote_path=pocket_table_path,
+                    binary_mode=transfer_binary,
+                )
+                tr = pyLSV2.TableReader()
+                pockets = tr.parse_table(local_recive_path)
+                spindel = list(
+                    filter(lambda pocket: pocket["P"] == spindel_lable, pockets)
+                )[0]
+                print("Current tool number {T} with name {TNAME}".format(**spindel))
