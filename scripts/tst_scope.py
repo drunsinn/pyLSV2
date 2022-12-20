2effbae98964a98f9174931faccc6a77ede408c3
Max <max@chp1.net>
2022-12-20 19:00:18 +0100

    simplify channel setup

--- scripts/tst_scope.py
+++ scripts/tst_scope.py
@@ -16,12 +16,15 @@ with pyLSV2.LSV2("localhost", port=19000, timeout=5, safe_mode=False) as con:
                 con.versions.control_version, con.versions.nc_version
             )
         )
+    
+    print("number of scope channels %d" % con.parameters.number_of_scope_channels)
+    print("max number of trace lines %d" % con.parameters.max_trace_line)
 
     channel_list = con.tst_read_scope_channels()
 
     for channel in channel_list:
         print("Channel No. {number} of type {type} : {name} supported signals: {axes_list}".format(**channel))
 
-    readings = con.tst_record_data(num_readings=10, intervall_us=3000)
+    readings = con.tst_record_data(num_readings=100, intervall_us=6000)
 
     print(len(readings))
